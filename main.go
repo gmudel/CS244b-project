c7d574411bc4f243b0a7be750a5319144bb4dc0f
Aman Bansal <aman0456b@gmail.com>
2022-05-20 15:17:07 -0700

    Take nodeId from command line

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"flads/ds"
 	"flads/ds/network"
 	"flads/ml"
+	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	numNodes := 3
-	curNodeId := 0
+	curNodeId, err := strconv.Atoi(os.Args[1])
+	if err != nil || curNodeId >= numNodes || curNodeId < 0 {
+		panic("Cannot get the node id or node id out or range")
+	}
+	fmt.Println(curNodeId)
 	mlp := &ml.DumbMLProcess{}
 	networkTable := map[int]string{ // nodeId : ipAddr
 		0: "localhost:7003",
@@ -20,7 +27,7 @@ func main() {
 	port := ":" + strings.Split(networkTable[curNodeId], ":")[1]
 	net := network.Network{}
 	net.Initialize(curNodeId, port, make([]network.Message, 0), networkTable)
-	err := net.Listen()
+	err = net.Listen()
 	if err != nil {
 		panic("Network not able to listen")
 	}
@@ -30,7 +37,7 @@ func main() {
 		numNodes,
 		mlp,
 		net,
-		3,
+		2,
 	)
 	x.Run()
 }
